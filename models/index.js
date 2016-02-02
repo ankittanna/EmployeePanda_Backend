e58485508622cdbbd730a974ced671096c6ee23d
Ankit <ankit.bharat.tanna@accenture.com>
2016-02-03 00:17:19 +0530

    EP: Models Index
    EP: Models Index

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 var modelsIndex = {
     menu: require('./MenuModel.js'),
-    order: require('./OrderModel.js')
+    order: require('./OrderModel.js'),
+    vendor: require('./VendorModel.js')
 };
 
 module.exports = modelsIndex;
