48ca87e582aa7c71411b112460052c8da4cbe818
Ankit <ankit.bharat.tanna@accenture.com>
2016-02-03 00:40:38 +0530

    EP: Adding Routes to Controllers
    EP: Adding Routes to Controllers

--- index.js
+++ index.js
@@ -25,6 +25,16 @@ app.use(function(req, res, next){
 // Connect here with MongoDB
 mongoose.connect('mongodb://localhost/employeepandadb');
 mongoose.connection.once('open', function(){
+    // Load the models
+    app.models = require('./models/index');
+    
+    // Load the routes
+    var routes = require('./routes/routes');
+    
+    _.each(routes, function(controller, route) {
+        app.use(route, controller(app, route));
+    });
+    
     console.log('Listening on port 3000');
     app.listen(3000);
 });
---
+++ routes/routes.js
@@ -0,0 +1,4 @@
+module.exports = {
+    '/order': require('./controllers/OrderController'),
+    '/vendor': require('./controllers/VendorController')
+};
