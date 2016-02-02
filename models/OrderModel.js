b18cc1eeb001059150ea97a0b714dfcad345f0b8
Ankit <ankit.bharat.tanna@accenture.com>
2016-02-03 00:16:07 +0530

    EP: Order Model
    EP: Order Model

--- models/OrderModel.js
+++ models/OrderModel.js
@@ -9,7 +9,26 @@ var OrderSchema = new mongoose.Schema({
     // vendor name
     // user phone number
     // order status - can be changed only by vendor
-    // 
+    username:{
+        type: String,
+        required: true
+    },
+    orderTime:{
+        type: Date,
+        required: true
+    },
+    vendor: {
+        type: String,
+        required: true
+    },
+    userPhoneNumber:{
+        type: String,
+        required: true
+    },
+    orderStatus:{
+        type: String,
+        required: true
+    }
 });
 
 // Export the model schema
